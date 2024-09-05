659ad3342cb1f4cbac4a8ec3b123e6070f500032
SameerShelarr <workingshelar@gmail.com>
2024-09-06 01:07:05 +0900

    Added Firebase setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // for connecting with Firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,4 +59,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.store)
+    implementation(libs.firebase.remote.config)
 }
